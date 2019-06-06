9589d090b608d6f8083d56afe0b7fdb5f51199c4
Evgeniy Altunin <20day02@rambler.ru>
2019-06-06 16:10:02 +0300

    создание второго уровня. Геморный биндинг.

--- webapp/modules/mainmodule/controller/mainController.controller.js
+++ webapp/modules/mainmodule/controller/mainController.controller.js
@@ -20,15 +20,6 @@ sap.ui.define([
         onPressMasterBack: function () {
             this.byId("SplitAppDemo").backMaster();
         },
-        goToProtectiveConstructions: function (oEvent) {
-            this.byId("SplitAppDemo").to(this.createId("catalog_ProtConstructions"));
-        },
-        goToFrameConstructions: function (oEvent) {
-            this.byId("SplitAppDemo").to(this.createId("catalog_FrameConstructions"));
-        },
-        goToStainlessSteelFurniture: function (oEvent) {
-            this.byId("SplitAppDemo").to(this.createId("catalog_StainlessSteelFurniture"));
-        },
         onPressDetailBack: function () {
             this.byId("SplitAppDemo").backDetail();
         },
@@ -37,18 +28,71 @@ sap.ui.define([
 
             this.byId("SplitAppDemo").toDetail(this.createId(sToPageId));
         },
+
+        //создание первого уровня меню
         onMenuItemPress: function (oEvent) {
             var oData = oEvent.getSource().getBindingContext("myModel").getObject();
+            this.navigateToMaster(oData);
+        },
+
+            //создание второго уровня меню
+        onMenuItemPress2: function (oEvent) {
+            var oData = oEvent.getSource().getBindingContext("myModel").getObject();
+            
+            var sPath = oEvent.getSource().getBindingContext("myModel").getPath();
+            var sChild = oData.child;
+            var oList_types = this.getView().byId("typeList");
+           // oList_types.bindElement({path:sPath+"/"+sChild, model: "myModel"});
+           var oTemplate = oList.getBindingInfo("items").template;
+           oList_types.bindItems("myModel>"+sPath+"/"+sChild, oTemplate);
+
+            this.navigateToMaster(oData);
+        },
+        //создание второго уровня меню
+        onMenuItemPress3: function (oEvent) {
+            var oData = oEvent.getSource().getBindingContext("myModel").getObject();
+            
+            var sPath = oEvent.getSource().getBindingContext("myModel").getPath();
+            var sChild = oData.child;
+            var oList_articles = this.getView().byId("typeList");
+            // oList__articles.bindElement({path:sPath+"/"+sChild, model: "myModel"});
+            var oTemplate = oList.getBindingInfo("items").template;
+            oList_articles.bindItems("myModel>"+sPath+"/"+sChild, oTemplate);
+
+            this.navigateToMaster(oData);
+        },
+        navigateToMaster: function(oData){
             if (oData.link) {
                 if (oData.master_option) {
                     this.byId("SplitAppDemo").toMaster(this.createId(oData.link));
                 }
-            }
-
-
+            } else {alert ("нет линка!");}
         },
         menuItemType: function(bType){
             return bType ? "Navigation" : "Active";
+        },
+        //создание второго уровня меню
+        onMenu2ItemPress: function (oEvent) {
+            var sPath = oEvent.getSource().getBindingContext("myModel").getPath();
+            console.log(sPath);
+            console.log(oEvent.getSource().getBindingContext("myModel").getObject());
+
+            var oData = oEvent.getSource().getBindingContext("myModel").getObject();
+            if (oData.link) {
+                if (oData.master_option) {
+                    this.byId("SplitAppDemo").toMaster(this.createId(oData.link));
+                } else {alert ("нет линка!");}
+            } else {alert ("нет линка!");}
+        },
+        menuItemType: function(bType){
+            return bType ? "Navigation" : "Active";
+        },
+
+        //создание третьего уровня меню
+        onTypeItemPress: function (oEvent) {
+            let sPath = oEvent.getSource().getBindingContext("myModel").getPath();
+            console.log(sPath);
+            console.log(oEvent.getSource().getBindingContext("myModel").getObject());
         }
     });
 
